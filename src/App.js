7667bda843de217cffdaba61df1a4ce38b04f672
Redouane Achouri <achouri.a.r@gmail.com>
2020-11-30 21:25:21 +0100

    Bootstapped app using Create React App and installed Bulma.io for styling

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Hello Rosetta!
+        </h1>
+        <p className="subtitle">
+          First interface of Rosetta using <strong>Bulma.io</strong>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
